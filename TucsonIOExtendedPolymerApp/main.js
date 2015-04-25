136f79278573c97aca39e3f027dc701c5c95c9f3
Dan Stormont <daniel.stormont@gmail.com>
2015-04-24 23:42:34 -0700

    Fixed tabs to start with a selection of the 27th.

--- TucsonIOExtendedPolymerApp/main.js
+++ TucsonIOExtendedPolymerApp/main.js
@@ -1,6 +1,8 @@
 // Listener to switch pages when tab selected.
 var tabs = document.querySelector('paper-tabs');
 
+tabs.selected = 0;
+
 tabs.addEventListener('core-select', function() {
   console.log("Selected: " + tabs.selected);
 });
