1f4bdd6fc7b1857ed44b93b6887b10470ef50929
Dan Stormont <daniel.stormont@gmail.com>
2015-05-02 01:21:46 -0700

    Changed to follow SPA example on polymer-project.org, but core-scaffold is not working!

--- TucsonIOExtendedPolymerApp/main.js
+++
@@ -1,8 +0,0 @@
-var template = document.querySelector('template[is="auto-binding"]');
-
-template.pages = [
-  {name: 'Wednesday, 27 May', hash: 'one'},
-  {name: 'Thursday, 28 May', hash: 'two'},
-  {name: 'Friday, 29 May', hash: 'three'},
-  {name: 'Saturday, 30 May', hash: 'four'}
-];
